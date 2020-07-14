6ec6b17754b780d8aa2a9159ece68ab17ea92718
SashaK1 <68186583+SashaK1@users.noreply.github.com>
2020-07-14 10:34:40 +0200

    Create 02_Datentyp.dart

---
+++ 02_Datentyp.dart
@@ -0,0 +1,10 @@
+void main() {
+
+  String meinName = "Sasha";
+  print("Mein Name ist $meinName");
+  double i = 4;
+String hallo = i.toString();
+  print(i);
+ int laenge = hallo.length;
+print(laenge);
+}
