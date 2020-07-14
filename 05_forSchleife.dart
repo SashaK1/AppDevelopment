00c4ebf3d583d699c3ed7e43b56993ef8989fe14
SashaK1 <68186583+SashaK1@users.noreply.github.com>
2020-07-14 11:38:32 +0200

    Create 05_forSchleife.dart

---
+++ 05_forSchleife.dart
@@ -0,0 +1,12 @@
+void main() {
+for(int limit=1; limit<5; limit++){
+  print("wir sind bei $limit");
+}
+}
+//oder
+void main() {
+  int limit = 5;
+for(int i=1; i<limit; i++){
+  print("wir sind bei $i");
+}
+}
